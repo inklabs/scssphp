5bc9673fb6f40d2de62fdebb54b816fa5ed057b1
Jamie Isaacs <pdt256@gmail.com>
2016-08-24 00:08:38 -0700

    Maintaining importPath structure to enable scss themes.

--- src/Compiler.php
+++ src/Compiler.php
@@ -3198,10 +3198,7 @@ class Compiler
             $this->importCache[$realPath] = $tree;
         }
 
-        $pi = pathinfo($path);
-        array_unshift($this->importPaths, $pi['dirname']);
         $this->compileChildrenNoReturn($tree->children, $out);
-        array_shift($this->importPaths);
     }
 
     /**
